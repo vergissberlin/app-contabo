8d893be4e8d6741b047a42ed7ae1cb47aeab7b87
André Lademann <andre.lademann@netresearch.de>
2022-10-21 11:31:33 +0200

    Add contabo opener

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar'
+import {StyleSheet, Text, View} from 'react-native'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Text style={styles.logo}>contabo</Text>
+            <StatusBar style="auto" />
+        </View >
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    logo: {
+        fontWeight: "bold",
+        fontSize: 50,
+        color: "#00aaeb",
+        textTransform: "uppercase",
+    }
+})
--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 Contabo ist ein Hoster aus Deutschland. Unter https://my.contabo.com bieten sie ihren Kunden ein Webinterface an, bei der die Kunden ihre Server verwalten können. Dieses Webinterface ist leider nicht sehr benutzerfreundlich und bietet auch nicht viele Funktionen. Deshalb habe ich mich entschieden, ein eigenes Interface zu schreiben, welches die Funktionen von Contabo erweitert und vereinfacht. Die App ist  mit React Native geschrieben und läuft auf Android und iOS.
 
+Ich bin kein Mitarbeiter von Contabo. Ich bin Kunde und habe die App für mich geschrieben, um jederzeit über den Status meiner Server informiert zu sein.
+
 ## Features
 
 Im ersten Release sind folgende Features enthalten:
